722c21d9fd296469c44d47241fae3022cfd6ce9c
Jackson <jackson@jacksonmoody.com>
2024-11-16 18:45:54 -0600

    Landing page

---
+++ src/app/fonts.ts
@@ -0,0 +1,12 @@
+import { Titan_One, Inter } from "next/font/google";
+
+export const titan = Titan_One({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,13 +1,30 @@
 import Image from "next/image";
-import logo from "@/app/images/logo.png";
+import avocado from "@/app/images/avocado.png";
+import { titan } from "@/app/fonts";
 
 export default function Landing() {
   return (
-    <div className="flex w-full items-center bg-white h-screen text-background">
-      <div className="m-auto">
-        <Image src={logo} alt="Monch" />
-        <h1 className="text-4xl font-bold">Welcome to Monch</h1>
-        <p className="text-lg">The best place to find your favorite recipes</p>
+    <div className="flex flex-col w-full items-center justify-center h-screen bg-light">
+      <div className="relative -top-20">
+        <div className={titan.className}>
+          <h2 className="text-center text-3xl">Introducing...</h2>
+          <div className="flex md:text-[12rem] text-8xl text-primary relative">
+            <Image
+              src={avocado}
+              alt="Avocado"
+              width="500"
+              className="absolute md:left-40 md:top-12 left-20 top-6 animate-bounce md:w-44 w-24"
+            />
+            <h1>M</h1>
+            <div className="md:w-40 w-24" />
+            <h1 className="tracking-widest">NCH</h1>
+          </div>
+        </div>
+        <div className="text-center">
+          <p className="text-2xl font-extrabold text-secondary pt-10">
+            Macro Tracking Made Easy
+          </p>
+        </div>
       </div>
     </div>
   );
